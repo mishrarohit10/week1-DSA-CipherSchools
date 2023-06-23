43bc1713517c36218f4c0b032c284f8fa53dfb24
mishrarohit10 <mishrarohit316x@gmail.com>
2023-06-23 11:37:34 +0530

    :sparkles: Completed day1
    finished day1

--- day1/PeopleMeetEachOther.java
+++ day1/PeopleMeetEachOther.java
@@ -3,8 +3,8 @@ package day1;
 import java.util.Scanner;
 
 class PeopleMeetEachOther {
-
-    public boolean isMeet(int p1, int p2, int s1, int s2) {
+//    complexity O(n)
+    public static boolean isMeet(int p1, int p2, int s1, int s2) {
         if (p1 < p2 && s1 <= s2) {
             return false;
         }
@@ -22,6 +22,21 @@ class PeopleMeetEachOther {
         return false;
     }
 
+//    complexity: O(1)
+    public static boolean isMeetOptimized(int p1, int p2, int s1, int s2) {
+        if (p1 < p2 && s1 <= s2) {
+            return false;
+        }
+        if (p1 > p2 && s1 >= s2) {
+            return false;
+        }
+
+        if((p1-p2)%(s2-s1)==0) {
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int p1 = in.nextInt();
@@ -29,7 +44,10 @@ class PeopleMeetEachOther {
         int p2 = in.nextInt();
         int s2 = in.nextInt();
         in.close();
-        boolean ans = new PeopleMeetEachOther().isMeet(p1, p2, s1, s2);
+        boolean ans = isMeet(p1, p2, s1, s2);
         System.out.println(ans ? "They met" : "They didnt met");
+
+        boolean ans2 = isMeetOptimized(p1, p2, s1, s2);
+        System.out.println(ans2 ? "They met" : "They didnt met");
     }
 }
